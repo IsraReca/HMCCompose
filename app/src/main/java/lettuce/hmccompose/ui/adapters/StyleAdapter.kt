91ea619f4bcd5b12e936e8dfea97131197087a3d
Israel Gajardo <israel.gajardo@carsales.com>
2022-08-22 14:16:44 -0400

    Adds StyleAdapter to ComplexText

--- app/src/main/java/lettuce/hmccompose/ui/adapters/StyleAdapter.kt
+++ app/src/main/java/lettuce/hmccompose/ui/adapters/StyleAdapter.kt
@@ -1,27 +1,194 @@
 package lettuce.hmccompose.ui.adapters
 
 import androidx.compose.ui.text.TextStyle
-import lettuce.hmccompose.ui.theme.CellHeaderText_Style
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import lettuce.hmccompose.data.complextext.FontType
+import lettuce.hmccompose.data.complextext.StylingComplexTextID
+import lettuce.hmccompose.ui.theme.*
 import lettuce.hmccompose.ui.theme.Default_Style
-import lettuce.hmccompose.ui.theme.Title1Text_Style
+import lettuce.hmccompose.ui.theme.Font
 
 class StyleAdapter {
     companion object {
-        private const val STYLE_TITTLE1TEXT = "title1Text"
-        private const val STYLE_CELLHEADERTEXT = "cellHeaderText"
-
-        fun getStyleById(id: String): TextStyle {
-            return when(id) {
-                STYLE_TITTLE1TEXT -> {
-                    Title1Text_Style
-                }
-                STYLE_CELLHEADERTEXT -> {
-                    CellHeaderText_Style
-                }
-                else -> {
-                    Default_Style
+        fun getStyleById(
+            styleId: String?,
+            typeId: String? = null
+        ): TextStyle {
+            StylingComplexTextID.from(styleId)?.let {
+                return when (it) {
+                    StylingComplexTextID.HEAD_LINE_TEXT -> TextStyle(
+                        fontFamily = Font.OpenSans,
+                        fontWeight = getFontWeightById(typeId),
+                        fontSize = 16.sp,
+                        color = Black
+                    )
+                    StylingComplexTextID.LARGE_TITLE_TEXT -> TextStyle(
+                        fontFamily = Font.OpenSans,
+                        fontWeight = getFontWeightById(typeId),
+                        fontSize = 18.sp,
+                        color = HeadingGrey700
+                    )
+                    StylingComplexTextID.SMALL_TEXT -> {
+                        // TODO: Add Missing Style
+                        Default_Style
+                    }
+                    StylingComplexTextID.TITLE_HEADER -> TextStyle(
+                        fontFamily = Font.OpenSans,
+                        fontWeight = getFontWeightById(typeId),
+                        fontSize = 20.sp,
+                        color = HeadingGrey700
+                    )
+                    StylingComplexTextID.HEAD_LINE_PLAIN_TEXT -> TextStyle(
+                        fontFamily = Font.OpenSans,
+                        fontWeight = getFontWeightById(typeId),
+                        fontSize = 14.sp,
+                        color = HeadingGrey700
+                    )
+                    StylingComplexTextID.CAPTION_TEXT -> {
+                        // TODO: To Implement
+                        Default_Style
+                    }
+                    StylingComplexTextID.CELL_SUBTITLE_TEXT -> TextStyle(
+                        fontFamily = Font.OpenSans,
+                        fontWeight = getFontWeightById(typeId),
+                        fontSize = 16.sp,
+                        color = HeadingGrey700
+                    )
+                    StylingComplexTextID.CELL_HEADER_TEXT -> TextStyle(
+                        fontFamily = Font.OpenSans,
+                        fontWeight = getFontWeightById(typeId),
+                        fontSize = 16.sp,
+                        color = HeadingGrey700
+                    )
+                    StylingComplexTextID.CELL_DETAIL_TEXT -> TextStyle(
+                        fontFamily = Font.OpenSans,
+                        fontWeight = getFontWeightById(typeId),
+                        fontSize = 14.sp,
+                        color = ParagraphGrey600
+                    )
+                    StylingComplexTextID.SECONDARY_TEXT -> {
+                        val frontWeight = getFontWeightById(typeId)
+                        val color = if (frontWeight == FontWeight.Bold) {
+                            HeadingGrey700
+                        } else {
+                            Black
+                        }
+                        TextStyle(
+                            fontFamily = Font.OpenSans,
+                            fontWeight = frontWeight,
+                            fontSize = 14.sp,
+                            color = color
+                        )
+                    }
+                    StylingComplexTextID.TITLE2_TEXT -> TextStyle(
+                        fontFamily = Font.OpenSans,
+                        fontWeight = getFontWeightById(typeId),
+                        fontSize = 16.sp,
+                        color = HeadingGrey700
+                    )
+                    StylingComplexTextID.BODY_TEXT -> TextStyle(
+                        fontFamily = Font.OpenSans,
+                        fontWeight = getFontWeightById(typeId),
+                        fontSize = 16.sp,
+                        color = HeadingGrey700
+                    )
+                    StylingComplexTextID.SUB_HEAD_TEXT -> TextStyle(
+                        fontFamily = Font.OpenSans,
+                        fontWeight = getFontWeightById(typeId),
+                        fontSize = 16.sp,
+                        color = Black
+                    )
+                    StylingComplexTextID.SECTION_HEADER -> TextStyle(
+                        fontFamily = Font.OpenSans,
+                        fontWeight = getFontWeightById(typeId),
+                        fontSize = 16.sp,
+                        color = Black
+                    )
+                    StylingComplexTextID.SECTION_PLAIN_WEB_LINK -> TextStyle(
+                        fontFamily = Font.OpenSans,
+                        fontWeight = getFontWeightById(typeId),
+                        fontSize = 16.sp,
+                        color = Black
+                    )
+                    StylingComplexTextID.JUSTIFY_TEXT_RIGHT -> TextStyle(
+                        fontFamily = Font.OpenSans,
+                        fontWeight = getFontWeightById(typeId),
+                        fontSize = 16.sp,
+                        color = Black
+                    )
+                    StylingComplexTextID.JUSTIFY_TEXT_LEFT -> TextStyle(
+                        fontFamily = Font.OpenSans,
+                        fontWeight = getFontWeightById(typeId),
+                        fontSize = 16.sp,
+                        color = Black
+                    )
+                    StylingComplexTextID.SECONDARY_CAPTION_TEXT -> TextStyle(
+                        fontFamily = Font.OpenSans,
+                        fontWeight = getFontWeightById(typeId),
+                        fontSize = 16.sp,
+                        color = Black
+                    )
+                    StylingComplexTextID.SELECT_BANNER_GRID_ITEM -> TextStyle(
+                        fontFamily = Font.OpenSans,
+                        fontWeight = getFontWeightById(typeId),
+                        fontSize = 16.sp,
+                        color = Black
+                    )
+                    StylingComplexTextID.SELECT_WHAT_IS_HEADER -> TextStyle(
+                        fontFamily = Font.OpenSans,
+                        fontWeight = getFontWeightById(typeId),
+                        fontSize = 16.sp,
+                        color = Black
+                    )
+                    StylingComplexTextID.SUB_HEAD_LINE_TEXT -> TextStyle(
+                        fontFamily = Font.OpenSans,
+                        fontWeight = getFontWeightById(typeId),
+                        fontSize = 16.sp,
+                        color = Black
+                    )
+                    StylingComplexTextID.ERROR_SUBHEAD_TEXT -> TextStyle(
+                        fontFamily = Font.OpenSans,
+                        fontWeight = getFontWeightById(typeId),
+                        fontSize = 16.sp,
+                        color = Black
+                    )
+                    StylingComplexTextID.WARNING_SUBHEAD_TEXT -> TextStyle(
+                        fontFamily = Font.OpenSans,
+                        fontWeight = getFontWeightById(typeId),
+                        fontSize = 16.sp,
+                        color = Black
+                    )
+                    StylingComplexTextID.GREEN_INFO_SUBHEAD_TEXT -> TextStyle(
+                        fontFamily = Font.OpenSans,
+                        fontWeight = getFontWeightById(typeId),
+                        fontSize = 16.sp,
+                        color = Black
+                    )
+                    StylingComplexTextID.SECTION_BODY -> TextStyle(
+                        fontFamily = Font.OpenSans,
+                        fontWeight = getFontWeightById(typeId),
+                        fontSize = 16.sp,
+                        color = Black
+                    )
                 }
             }
+            return Default_Style
+        }
+
+        private fun getFontWeightById(typeId: String?): FontWeight {
+            FontType.from(typeId)?.let {
+                return when (it) {
+                    FontType.REGULAR_FONT -> FontWeight.Normal
+                    FontType.BOLD_FONT -> FontWeight.Bold
+                    FontType.EXTRA_BOLD_FONT -> FontWeight.ExtraBold
+                    FontType.SEMI_BOLD_FONT -> FontWeight.SemiBold
+                    FontType.LIGHT_FONT -> FontWeight.Light
+                    FontType.MEDIUM_FONT -> FontWeight.Medium
+                    FontType.BLACK_FONT -> FontWeight.Black
+                }
+            }
+            return FontWeight.Normal
         }
     }
 }
